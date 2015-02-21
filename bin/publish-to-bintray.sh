31d869d846f2a0fd44eec23c894e68559b968292
Ron Kurr <rkurr@transparent.com>
2015-02-21 17:18:45 -0500

    Altered the published package

--- bin/publish-to-bintray.sh
+++ bin/publish-to-bintray.sh
@@ -19,6 +19,6 @@ curl --include --request DELETE --user "kurron:${KEY}" --header "Content-Type: a
 
 curl --include --request POST --user "kurron:${KEY}" --header "Content-Type: application/json" https://api.bintray.com/packages/kurron/maven/java-tools/versions --data "{ \"name\": \"${RELEASE}\", \"desc\": \"New bits!\" }" 
 
-curl --include --request PUT --user "kurron:${KEY}" --header "X-Bintray-Package:java-tools" --header "X-Bintray-Version:${RELEASE}" --header "Content-Type: application/octet-stream" --header "Accept:application/json" --data-binary @build/libs/tools-java-0.0.0-SNAPSHOT.jar https://api.bintray.com/maven/kurron/maven/java-tools/org.kurron.example/tools-java/${RELEASE}/tools-java-${RELEASE}.jar
+curl --include --request PUT --user "kurron:${KEY}" --header "X-Bintray-Package:java-tools" --header "X-Bintray-Version:${RELEASE}" --header "Content-Type: application/octet-stream" --header "Accept:application/json" --data-binary @build/libs/tools-java-0.0.0-SNAPSHOT.jar https://api.bintray.com/maven/kurron/maven/java-tools/org.kurron/tools-java/${RELEASE}/tools-java-${RELEASE}.jar
 
-curl --verbose --request POST --user "kurron:${KEY}" --header "Content-Type: application/json" https://api.bintray.com/content/kurron/maven/java-tools/${RELEASE}/publish 
+curl --include --request POST --user "kurron:${KEY}" --header "Content-Type: application/json" https://api.bintray.com/content/kurron/maven/java-tools/${RELEASE}/publish 
